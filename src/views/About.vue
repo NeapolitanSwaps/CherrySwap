6bf6a90ad656465d1178589d65f4de55591a759e
Christopher Maree <christopher.maree@gmail.com>
2019-08-23 22:34:37 +0200

    Removed uneeded files

--- src/views/About.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="about">
-      <div class="md-layout">
-    <div class="md-layout-item">a</div>
-    <div class="md-layout-item">b</div>
-    <div class="md-layout-item">c</div>
-  </div>
-  </div>
-</template>
