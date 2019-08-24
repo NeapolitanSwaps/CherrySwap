1aae2e57dbd7ea5c398d1ffac54995d251f51698
Christopher Maree <christopher.maree@gmail.com>
2019-08-24 10:33:01 +0200

    Added more chart functionality

--- src/components/SwapPlot.vue
+++ src/components/SwapPlot.vue
@@ -1,8 +1,27 @@
 <template>
   <div>
-    a
-    <vue-plotly :data="plotData" :layout="plotLayout" />
-    {{plotData}}
+    <md-card style="background:white">
+      <md-card-header>
+        <div class="md-layout">
+          <div class="md-layout-item">
+            <div class="md-title" style="padding-left:15px">
+              <b>Market View</b>
+            </div>
+          </div>
+          <div class="md-layout-item text-right" style="text-align:right">
+            <div class="md-title">
+              <b>Current APR:</b>
+              <span
+                style="color:#2DC4B6"
+              >{{(plotData[0].y[plotData[0].y.length-1]).toFixed(4)}}% APR</span>
+            </div>
+          </div>
+        </div>
+
+        <hr />
+      </md-card-header>
+      <vue-plotly :data="plotData" :layout="plotLayout" :options="plotOptions" />
+    </md-card>
   </div>
 </template>
 
@@ -25,75 +44,119 @@ export default {
           line: { shape: "spline" }
         }
       ],
-      plotLayout: {
-        shapes: [
-          {
-            type: "line",
-            x0: 0,
-            y0: 20,
-            x1: 200,
-            y1: 20,
-            line: {
-              color: "rgb(50, 171, 96)",
-              width: 4,
-              dash: "dashdot"
-            }
-          },
-          {
-            type: "line",
-            x0: 60,
-            y0: 10,
-            x1: 60,
-            y1: 30,
-            line: {
-              color: "rgb(50, 171, 96)",
-              width: 4,
-              dash: "dashdot"
-            }
-          },
-          {
-            type: "line",
-            x0: 180,
-            y0: 10,
-            x1: 180,
-            y1: 30,
-            line: {
-              color: "rgb(50, 171, 96)",
-              width: 4,
-              dash: "dashdot"
-            }
-          }
-        ],
-        annotations: [
-          {
-            x: 57,
-            y: 25,
-            xref: "x",
-            yref: "2",
-            text: "Start",
-            ax: 0,
-            ay: -40,
-            textangle: "-90"
-          }
-        ]
+      plotOptions: {
+        responsive: true,
+        showLink: false,
+        displayModeBar: false
       }
     };
   },
   methods: {},
   mounted() {
-    this.plotData[0].x = Array.from(Array(180).keys());
+    this.plotData[0].x = Array.from(Array(200).keys());
     setInterval(() => {
-      this.plotData[0].y.push(Math.random() * 10 + 15);
-    }, 500);
+      if (this.plotData[0].y.length < 200) {
+        this.plotData[0].y.push(
+          Math.random() * 8 -
+            3.7 +
+            this.plotData[0].y[this.plotData[0].y.length - 1]
+        );
+      }
+    }, 100);
   },
 
   computed: {
     // ...mapState(["etherscanBase"]),
-    // plotDataz() {
-    //   let value = 10;
-    //   setInterval(this.addValueToPlot(), 1000);
-    //   return this.interestRate;
-    // }
+    plotLayout() {
+      return {
+        yaxis: {
+          title: "% APR",
+          showline: false
+        },
+        paper_bgcolor: "rgba(0,0,0,0)",
+        plot_bgcolor: "rgba(0,0,0,0)",
+        shapes: [
+          {
+            type: "line",
+            x0: 0,
+            y0: this.plotData[0].y.length >= 60 ? this.plotData[0].y[60] : 20,
+            x1: 200,
+            y1: this.plotData[0].y.length >= 60 ? this.plotData[0].y[60] : 20,
+            line: {
+              color: "#D6D6D6",
+              width: this.plotData[0].y.length >= 60 ? 2 : 0,
+              dash: "dot"
+            }
+          },
+          {
+            type: "line",
+            x0: 60,
+            y0:
+              Math.min(...this.plotData[0].y) < 10
+                ? Math.min(...this.plotData[0].y)
+                : 10,
+            x1: 60,
+            y1:
+              Math.max(...this.plotData[0].y) > 30
+                ? Math.max(...this.plotData[0].y)
+                : 30,
+            line: {
+              color: "#D6D6D6",
+              width: 2,
+              dash: "dot"
+            }
+          },
+          {
+            type: "line",
+            x0: 180,
+            y0:
+              Math.min(...this.plotData[0].y) < 10
+                ? Math.min(...this.plotData[0].y)
+                : 10,
+            x1: 180,
+            y1:
+              Math.max(...this.plotData[0].y) > 30
+                ? Math.max(...this.plotData[0].y)
+                : 30,
+            line: {
+              color: "#D6D6D6",
+              width: 2,
+              dash: "dot"
+            }
+          }
+        ],
+        annotations: [
+          {
+            x: 58,
+            y: 10,
+            text: "Swap Start (t1)",
+            ax: 0,
+            ay: -40,
+            textangle: "-90"
+          },
+          {
+            x: 178,
+            y: 10,
+            text: "Swap End (t2)",
+            ax: 0,
+            ay: -40,
+            textangle: "-90"
+          },
+          {
+            x: 80,
+            y:
+              this.plotData[0].y.length >= 60 ? this.plotData[0].y[60] + 3 : 20,
+            text:
+              this.plotData[0].y.length >= 60
+                ? "Fixed Rate(long pays, short recives)"
+                : "",
+            ax: 0,
+            ay: -40,
+            showarrow: false
+          }
+        ]
+      };
+    }
   }
 };
 </script>
