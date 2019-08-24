e431b1d334dbde494dfce50740b31638f8d61698
Christopher Maree <christopher.maree@gmail.com>
2019-08-24 09:16:27 +0200

    Added initial plot

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Create from './views/Create.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/create',
+      name: 'create',
+      component: Create
     },
   ]
 })
