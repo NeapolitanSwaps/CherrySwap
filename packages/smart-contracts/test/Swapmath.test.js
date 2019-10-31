92528a94a4ae430d0a5183a673221a6a91dd9460
Christopher Maree <christopher.maree@gmail.com>
2019-10-31 18:25:07 +0200

    Updated unit tests

--- packages/smart-contracts/test/Swapmath.test.js
+++ packages/smart-contracts/test/Swapmath.test.js
@@ -19,6 +19,7 @@ const Swapmath = artifacts.require("SwapMath");
 let secondsPerYear = 60 * 60 * 24 * 365
 let blocksPerYear = 4 * 60 * 24 * 365
 let borrowRatePerBLock = 29482138861
+let nominal = 100 * 10 ** 18
 
 let Decimal = require("decimal.js");
 Decimal.set({
@@ -43,24 +44,36 @@ contract("SwapMath 🔬", ([contractOwner, random]) => {
     });
     context("Helper Maths", function () {
 
-        for (let percent = 0; percent < 10; percent++) {
-            let x = (new BigNumber(percent)).dividedBy(100).times(EXP_MAX.minus(EXP_MIN)).plus(EXP_MIN);
 
-            it(`Function optimalExp(${x.toFixed()})`, async function () {
+
+        it(`Exponentual calculation loop`, async function () {
+            // loop over a range within the accessable range of the function from EXP_Min to EXP_MAX
+            for (let percent = 0; percent < 10; percent++) {
+                let x = (new BigNumber(percent)).dividedBy(100).times(EXP_MAX.minus(EXP_MIN)).plus(EXP_MIN);
                 let fixedPoint = await this.swapmath.optimalExp.call(FIXED_1.times(x).toFixed(0));
                 let floatPoint = new BigNumber(Decimal(x.toFixed()).exp().times(FIXED_1.toFixed()).toFixed());
-                
+
                 let ratio = new BigNumber(fixedPoint).dividedBy(floatPoint);
                 assert(ratio.isGreaterThanOrEqualTo("0.999999999999999") && ratio.isLessThanOrEqualTo("1.000000000000001"), `ratio = ${ratio.toFixed()}`);
-            });
-        }
+            }
+        });
 
-        it("Can correctly capatalization function", async function () {
+
+        it("Can correctly find capatalization function", async function () {
             let expectedValue = new BigNumber(Math.exp((borrowRatePerBLock / 10 ** 18) * blocksPerYear) * 10 ** 18)
             let contractValue = await this.swapmath.capFunction.call(borrowRatePerBLock, 0, secondsPerYear);
 
             let ratio = new BigNumber(expectedValue).dividedBy(contractValue);
             assert(ratio.isGreaterThanOrEqualTo("0.999999999999999") && ratio.isLessThanOrEqualTo("1.000000000000001"), `Cap function incorrectly calculated`);
         })
+
+        it("Can correctly calculate future value of NACC position", async function () {
+            let capFunction = new BigNumber(Math.exp((borrowRatePerBLock / 10 ** 18) * blocksPerYear))
+            let expectedValue = capFunction.multipliedBy(nominal)
+            let contractValue = await this.swapmath.futureValue.call(nominal.toString(), borrowRatePerBLock, 0, secondsPerYear);
+            let ratio = new BigNumber(expectedValue).dividedBy(contractValue);
+            assert(ratio.isGreaterThanOrEqualTo("0.999999999999999") && ratio.isLessThanOrEqualTo("1.000000000000001"), `Cap function incorrectly calculated`);
+        })
+
     })
 })
