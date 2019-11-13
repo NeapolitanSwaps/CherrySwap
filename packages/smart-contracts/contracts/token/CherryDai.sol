5d13a331b1f75f2e263b33319800264eb4d4c2d3
Haythem Sellami <sellamihaythem96@gmail.com>
2019-11-13 22:25:45 +0100

    CherryDAI token contract & unit tests

---
+++ packages/smart-contracts/contracts/token/CherryDai.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.0;
+
+import "@openzeppelin/upgrades/contracts/Initializable.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20Mintable.sol";
+
+contract CherryDai is Initializable, ERC20Detailed, ERC20Mintable {
+
+  function initialize(address _sender) public initializer {
+    // need a better name & symbol for sure
+    ERC20Detailed.initialize("Cherry Dai", "CHD", 18);
+
+    ERC20Mintable.initialize(_sender);
+  }
+
+}
