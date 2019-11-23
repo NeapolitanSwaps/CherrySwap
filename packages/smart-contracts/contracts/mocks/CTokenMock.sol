4856904e51023e2410f6ac0da17093a12dd95f86
Christopher Maree <christopher.maree@gmail.com>
2019-11-23 17:00:06 +0200

    Updated contract mock interfaces

--- packages/smart-contracts/contracts/mocks/CTokenMock.sol
+++ packages/smart-contracts/contracts/mocks/CTokenMock.sol
@@ -5,28 +5,34 @@ import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol
 
 contract CTokenMock is ERC20 {
   
-  function mint(uint mintAmount) public returns(uint256) {
+  function mint(uint mintAmount) public returns(uint) {
     _mint(msg.sender, mintAmount);
     return 0;
   }
 
-  function redeem(uint redeemTokens) public returns(uint256) {
-    uint256 senderBalance = balanceOf(msg.sender);
+  function redeem(uint redeemTokens) public returns(uint) {
+    uint senderBalance = balanceOf(msg.sender);
     transfer(msg.sender, senderBalance);
     return 0;
   }
 
   function supplyRatePerBlock() public pure returns (uint) {
-    return 29482138861;
+    return 24206647178;
   }
 
-  function incrementSupplyRatePerBlock() public pure returns (uint) {
-    uint current = supplyRatePerBlock();
-    return current + 5760;
+  function getCash() public pure returns (uint256) {
+    return 12191772623652836238229233;
+  } 
+
+  function totalReserve() public pure returns (uint256) {
+    return 123569305920595200177091;
   }
 
-  function decrementSupplyRatePerBlock() public pure returns (uint) {
-    uint current = supplyRatePerBlock();
-    return current - 5760;
+  function totalReserves() public pure returns (uint256) {
+    return 123569305920595200177091;
+  }
+
+  function totalSupply() public pure returns (uint256) {
+    return 112835765547222288;
   }
 }
